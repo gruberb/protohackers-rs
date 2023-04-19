4b517ae5a53f77e3fcef9016302f5f4c72609dd6
Bastian Gruber <foreach@me.com>
2023-04-19 14:15:48 +0200

    Ports, Docker

--- problem_01/src/main.rs
+++ problem_01/src/main.rs
@@ -1,21 +1,27 @@
-use tokio::net::TcpListener;
 use tokio::io::{AsyncReadExt, AsyncWriteExt};
+use tokio::net::TcpListener;
 
 #[tokio::main]
 async fn main() -> Result<(), Box<dyn std::error::Error>> {
-    let listener = TcpListener::bind("127.0.0.1:4040").await?;
-
+    env_logger::init();
+    let listener = TcpListener::bind("0.0.0.0:8080").await?;
+    log::info!("Start TCP server");
     loop {
         let (mut socket, _) = listener.accept().await?;
 
         tokio::spawn(async move {
             let mut buf = [0; 1024];
-            
-            loop {
 
+            loop {
                 let n = match socket.read(&mut buf).await {
-                    Ok(n) if n == 0 => return,
-                    Ok(n) => n,
+                    Ok(n) if n == 0 => {
+                        log::info!("Receiving echo: {}", n);
+                        return;
+                    }
+                    Ok(n) => {
+                        log::info!("Receiving echo: {}", n);
+                        n
+                    }
                     Err(e) => {
                         log::error!("failed to read from socket; err = {:?}", e);
                         return;
